bbafa078eb651a45326c151e77bc59e3d3b7cca9
Greg Poslov <grig.skamsk@mail.ru>
2022-09-20 22:38:37 +0500

    Added scanner

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class BonusMilesService {
+    public int calculate(int cost) {
+        Scanner not = new Scanner(System.in); //Сканер количества билетов
+        System.out.println("Количество биллетов:");
+        int numberOfTickets = not.nextInt(); // Количество билетов
+        int bonusMiles = (cost * numberOfTickets) / 20;
+        return bonusMiles;
+    }
 }
