9b5dbde9270f5947206d00e126dc46557c19c590
Greg Poslov <grig.skamsk@mail.ru>
2022-09-26 07:42:46 +0500

    Added commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
+        int price = 10_000;//Стоимость билетов
         int miles = service.calculate(price);
         System.out.println("Количество бонусов: " + miles);
     }
